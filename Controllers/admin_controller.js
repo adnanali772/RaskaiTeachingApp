ee8e958aa182bfbe685a7a495e362e899aaa837f
adnan <adnan@adnans-MacBook-Pro.local>
2024-01-12 12:09:25 +0530

    finally_PaginationCompleted

--- Controllers/admin_controller.js
+++ Controllers/admin_controller.js
@@ -12,15 +12,14 @@ exports.adminPage = async (req, res)=> {
     const { _id, name, email } = req.session.user;
     const capitalizedName = capitalizeFirstLetter(name);
    
-        const page = parseInt(req.query.page) || 1;
-        const pageSize = parseInt(req.query.pageSize) || 10;
-        console.log("page number ==", page, pageSize);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 7;
+    const skip = (page - 1) * pageSize;
+    const users = await usermoadal.find().sort({ _id: -1 }).skip(skip).limit(pageSize);
 
-        const skip = (page - 1) * pageSize;
-
-        const users = await usermoadal.find().sort({ _id: -1 }).skip(skip).limit(pageSize);
-        console.log("user afer list ==> ", users)
-        res.render('adminpage', {capitalizedName, users});
-        // res.status(200).json({ users, currentPage: page, pageSize });
+    const totalUsers = await usermoadal.countDocuments();
+    const totalPages = Math.ceil(totalUsers / pageSize);
+    
+    res.render('adminpage', { totalPages, currentPage: page,pageSize, capitalizedName, users });
 
 }
